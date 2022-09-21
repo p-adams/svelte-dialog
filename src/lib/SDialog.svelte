561dff75585ad5b1ba5c1f369f142926deab39b8
Paul Adams <ghusiyy@gmail.com>
2022-09-20 19:30:51 -0700

    init svelte dialog component

---
+++ src/lib/SDialog.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let dialog: HTMLDialogElement = null;
+</script>
+
+<dialog bind:this={dialog}>
+  <p>test</p>
+  <button on:click={() => dialog.close()}> hide </button>
+</dialog>
+
+<button on:click={() => dialog.showModal()}> show </button>
+
+<style>
+</style>
